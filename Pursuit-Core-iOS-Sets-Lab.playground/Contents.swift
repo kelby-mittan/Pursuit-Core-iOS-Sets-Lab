6401042d8c8df4b993a66cf2f108a01aacb9f3db
Kelby Mittan <kelbymittan@Kelbys-MacBook-Pro.local>
2019-10-08 16:11:25 -0400

    answers up to 4

--- Pursuit-Core-iOS-Sets-Lab.playground/Contents.swift
+++ Pursuit-Core-iOS-Sets-Lab.playground/Contents.swift
@@ -9,22 +9,56 @@ import UIKit
 let numbers = [1,1,2,4,4,4,6,6,7,8]
 
 var numbersWithNoDuplicates = [Int]()
-
+var noDuplicateSet = Set(numbers)
+//print(noDuplicateSet.sorted())
+for num in noDuplicateSet.sorted() {
+    numbersWithNoDuplicates.append(num)
+}
+//print(numbersWithNoDuplicates)
 // Your code here
 
-//assert(numbersWithNoDuplicates == [1,2,4,6,7,8], "Was expecting [1,2,4,6,7,8], but got \(numbersWithNoDuplicates)")
+assert(numbersWithNoDuplicates == [1,2,4,6,7,8], "Was expecting [1,2,4,6,7,8], but got \(numbersWithNoDuplicates)")
 
 // Questions Two
 
 // Create a new array scoresThatAppearOnce that has all the elements from scores that appear exactly once.  It should be in the same order as the original.
 
 let scores = [1, 77, 83, 32, 77, 77, 83, 32, 99]
-
+var noNumArray = [Int]()
+var uniqueScore = Set<Int>() // 1, 99
+var removedNum: Set<Int> = [] // 77, 83 32
 var scoresThatAppearOnce = [Int]()
+for score in scores {
+    if uniqueScore.contains(score) || removedNum.contains(score) {
+        uniqueScore.remove(score)
+        removedNum.insert(score)
+    } else {
+        uniqueScore.insert(score)
+    }
+}
+for num in scores where uniqueScore.contains(num) {
+    scoresThatAppearOnce.append(num)
+}
+print(scoresThatAppearOnce)
+noNumArray = Array(uniqueScore)
+//print(scores)
+//print(noNumArray.sorted())
+//for num in scores {
+//    if uniqueSet.contains(num) {
+//        uniqueSet.remove(num)
+//    }
+//    noNumArray.append(num)
+//}
+
+//print(noNumArray)
+//print(scores)
+//print(uniqueScore.sorted())
+//var noNumbersTwice = Set(scores.sorted())
+
 
 // Your code here
 
-//assert(scoresThatAppearOnce == [1, 99], "Was expecting [1, 99], but got \(scoresThatAppearOnce)")
+assert(scoresThatAppearOnce == [1, 99], "Was expecting [1, 99], but got \(scoresThatAppearOnce)")
 
 // Question Three
 
@@ -37,6 +71,12 @@ let arrTwo = [3,4,5,6,7]
 
 var arrThree: [Int] = []
 
+let arrOneSet = Set(arrOne.sorted())
+let arrTwoSet = Set(arrTwo)
+
+let arrThreeSet = arrOneSet.union(arrTwoSet)
+arrThree = Array(arrThreeSet.sorted())
+print(arrThree)
 // Your code here
 
 //assert(arrThree == [1,2,3,4,5,6,7], "Was expecting [1,2,3,4,5,6,7], but got \(arrThree)")
@@ -50,9 +90,15 @@ let arrFive = [3,4,5,6,7]
 
 var arrSix: [Int] = []
 
+let arrFourSet = Set(arrFour)
+let arrFiveSet = Set(arrFive)
+
+let arrSixSet = arrFourSet.intersection(arrFiveSet)
+arrSix = Array(arrSixSet.sorted())
+print(arrSix)
 // Your code here
 
-//assert(arrSix == [3,4,5], "Was expecting [3,4,5], but got \(arrSix)")
+assert(arrSix == [3,4,5], "Was expecting [3,4,5], but got \(arrSix)")
 
 // Question Four
 
